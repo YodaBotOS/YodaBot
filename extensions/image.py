410a73a293e99e0a8c0eaa411dbd222add55a183
proguy914629 <github@proguy.me>
2023-01-03 21:49:58 +0700

    Fix style autocomplete
    dumb error

--- extensions/image.py
+++ extensions/image.py
@@ -529,7 +529,8 @@ class Image(commands.Cog):
     @gen_art_style_slash.autocomplete('style')
     async def gen_art_style_slash_autocomplete(self, interaction: discord.Interaction, current: str):
         if not current:
-            return (await self.image.style.get_styles(raw=True))[:25]
+            return [app_commands.Choice(name=style.name, value=style.name) 
+                    for style in await self.image.style.get_styles(raw=True)][:25]
         
         styles = []
         
